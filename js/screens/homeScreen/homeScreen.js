28fd2e94d6029cbe581b6ef036af6396bf3d6c49
shei347469 <imrul.hasan@exact.com>
2017-05-08 01:25:16 +0800

    native-base lib added

--- js/screens/homeScreen/homeScreen.js
+++ js/screens/homeScreen/homeScreen.js
@@ -1,8 +1,10 @@
 
 import React, {Component, PropTypes} from 'react';
-import {StyleSheet, View, Text } from 'react-native';
+import {StyleSheet, Image } from 'react-native';
 
-// import Template from '../template/';
+
+import { Container, Header, Title, Button, Left, Right, Body, Icon, Text, ListItem, CheckBox, Fab, Footer, FooterTab, Badge,
+     Content, Card, CardItem, Thumbnail } from 'native-base';
 
 
 const styles = StyleSheet.create({
@@ -11,13 +13,80 @@ const styles = StyleSheet.create({
 });
 
 class HomeScreen extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            active: 'true'
+        };
+    }
     render() {
         return (
-            <View style={{flex: 1, flexDirection: 'row'}}>
-                <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-                <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
-                <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
-            </View>
+            <Container>
+                <Header>
+                    <Left>
+                        <Button transparent>
+                            <Icon name='menu' />
+                        </Button>
+                    </Left>
+                    <Body>
+                        <Title>Header</Title>
+                    </Body>
+                </Header>
+                <Content>
+                    <Card >
+                        <CardItem>
+                            <Left>
+                               <Icon name='md-school' />
+                                <Body>
+                                    <Text>NativeBase</Text>
+                                    <Text note>GeekyAnts</Text>
+                                </Body>
+                            </Left>
+                          </CardItem>
+                          <CardItem cardBody>
+                              <Image style={{width: 360, height: 200}}
+                              source={{uri: 'https://scotch.io/wp-content/uploads/2016/07/ckNko8BBRIGjBKsR1GV9_Banner.jpg'}} />
+                          </CardItem>
+                          <CardItem>
+                              <Button transparent>
+                                  <Icon active name="thumbs-up" />
+                                  <Text>12 Likes</Text>
+                              </Button>
+                              <Button transparent>
+                                  <Icon active name="chatbubbles" />
+                                  <Text>4 Comments</Text>
+                              </Button>
+                              <Text>11h ago</Text>
+                        </CardItem>
+                   </Card>
+                   <Button full>
+                        <Text>Click Me! </Text>
+                    </Button>
+                </Content>
+
+                <Footer >
+                    <FooterTab>
+                          <Button badge vertical>
+                              <Badge><Text>2</Text></Badge>
+                              <Icon name="apps" />
+                              <Text>Apps</Text>
+                          </Button>
+                          <Button>
+                              <Icon name="camera" />
+                              <Text>Camera</Text>
+                          </Button>
+                          <Button active badge vertical>
+                              <Badge ><Text>51</Text></Badge>
+                              <Icon active name="navigate" />
+                              <Text>Navigate</Text>
+                          </Button>
+                          <Button>
+                              <Icon name="person" />
+                              <Text>Contact</Text>
+                          </Button>
+                      </FooterTab>
+                </Footer>
+            </Container>
         );
     }
 }
